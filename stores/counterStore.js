f4d9ba0e317a6326fbd1ce5eb91efb27fd344425
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-18 06:15:56 +0000

    Automatically backed up by Learn

--- stores/counterStore.js
+++ stores/counterStore.js
@@ -4,8 +4,8 @@ import Store from './Store';
 class CounterStore extends Store{
   // Your implementation here.
   // Hint: extend the Store class!
-  increment = () => {};
-  decrement = () => {};
+  increment(){};
+  decrement(){};
 }
 
 const counterStore = new CounterStore();
