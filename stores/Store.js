ea7459f91588c24bab05f72760140203ba9d4358
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-18 05:22:07 +0000

    Automatically backed up by Learn

--- stores/Store.js
+++ stores/Store.js
@@ -5,6 +5,14 @@ class Store {
   constructor(initialState){
     this.state = initialState;
   }
+
+  setState(state){
+    this.state = state;
+  }
+
+  getState(){
+    return this.state;
+  }
 }
 
 export default Store;
