6beb673d9f6c062461ef79db2229ff74799897fe
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-18 08:33:35 +0000

    Done.

--- components/App.js
+++ components/App.js
@@ -1,27 +1,42 @@
 import React from 'react';
+import counterStore from '../stores/counterStore';
+import actions from '../actions';
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      // Your implementation here.
+      counter:counterStore.getState()
     };
   }
   componentDidMount () {
-    // Your implementation here.
+    this.removeListener = counterStore.addListener((state) => {
+      this.setState({counter:state});
+    });
   }
   componentWillUnmount () {
-    // Your implementation here.
+    this.removeListener();
   }
+
+  decrement(ev){
+    ev.preventDefault();
+    actions.decrement();
+  }
+
+  increment(ev){
+    ev.preventDefault();
+    actions.increment();
+  }
+
   render () {
     return (
       <div className='app'>
-        <h1 className='counter'></h1>
+        <h1 className='counter'>{this.state.counter}</h1>
         <div className='actions'>
-          <button className='decrement'>
+          <button className='decrement' onClick={this.decrement}>
             -
           </button>
-          <button className='increment'>
+          <button className='increment' onClick={this.increment}>
             +
           </button>
         </div>
--- stores/counterStore.js
+++ stores/counterStore.js
@@ -1,6 +1,21 @@
-class CounterStore {
+import React from 'react';
+import Store from './Store';
+
+class CounterStore extends Store{
   // Your implementation here.
   // Hint: extend the Store class!
+  constructor(props){
+    super(props);
+    this.state = 0;
+  }
+  increment(){
+    this.setState(this.getState() + 1);
+  }
+
+  decrement(){
+    this.setState(this.getState() - 1);
+  }
+
 }
 
 const counterStore = new CounterStore();
