dc30fa601259342435ddb05e88b56088b7846e18
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-18 05:19:58 +0000

    Automatically backed up by Learn

--- stores/Store.js
+++ stores/Store.js
@@ -2,6 +2,9 @@ class Store {
   // Your implementation here.
   // Hint: Our <App /> component won't directly use this store, but instead the
   // CounterStore is going to inherit from it.
+  constructor(){
+    
+  }
 }
 
 export default Store;
